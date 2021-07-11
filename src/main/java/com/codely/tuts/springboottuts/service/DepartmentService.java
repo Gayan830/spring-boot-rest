1d7b43cdad52b2b0716316e5bd8ab0f997274139
gayan830 <malshan95104@gmail.com>
2021-07-11 21:48:53 +0530

    ADD: new api methods

--- src/main/java/com/codely/tuts/springboottuts/service/DepartmentService.java
+++ src/main/java/com/codely/tuts/springboottuts/service/DepartmentService.java
@@ -10,5 +10,11 @@ public interface DepartmentService {
 
     public List<Department> fetchDepartmentList();
 
-    Department fetchDepartment(Long departmentId);
+    public Department fetchDepartment(Long departmentId);
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+
+    public Department fetchDepartmentByName(String departmentName);
 }
